110bab6e33e67d01dc66bde1918dbe82c6331769
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-31 14:28:53 -0500

    feat(category): auditing add JpaAuditingConfig and BaseEntity create update

---
+++ src/main/java/hub/com/api_store/util/auditing/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package hub.com.api_store.util.auditing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
