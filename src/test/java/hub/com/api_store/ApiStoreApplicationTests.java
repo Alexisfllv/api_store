d646fac791fce3154d431bbf2a1b2f3dc0d87002
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-31 08:36:44 -0500

    chore(setup): base

---
+++ src/test/java/hub/com/api_store/ApiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.com.api_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
