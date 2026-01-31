8a9d25765a52954f3017a5e1b22bf993ad8510b5
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-31 13:59:48 -0500

    feat(category): add entity Category

---
+++ src/main/java/hub/com/api_store/entity/Category.java
@@ -0,0 +1,35 @@
+package hub.com.api_store.entity;
+
+
+import hub.com.api_store.nums.CategoryStatus;
+import hub.com.api_store.util.auditing.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories", indexes = {
+        @Index(name = "idx_category_name", columnList = "name"),
+        @Index(name = "idx_category_status", columnList = "status")
+})
+public class Category extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryStatus status;
+}
