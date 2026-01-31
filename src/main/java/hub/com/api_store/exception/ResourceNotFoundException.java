cc5779db76fba3e3cabf4e87bded10b0ce62ba49
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-31 14:09:32 -0500

    chore(exception): add ResourceNotFoundException

---
+++ src/main/java/hub/com/api_store/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package hub.com.api_store.exception;
+
+public class ResourceNotFoundException extends BaseException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
