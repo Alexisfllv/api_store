65eb078b817f2a89aa521b184d83576058732e7f
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-01 16:15:07 -0500

    feat(category): CategoryService add GET getPageListCategory

--- src/main/java/hub/com/api_store/service/CategoryService.java
+++ src/main/java/hub/com/api_store/service/CategoryService.java
@@ -1,8 +1,11 @@
 package hub.com.api_store.service;
 
 import hub.com.api_store.dto.category.CategoryDTOResponse;
+import hub.com.api_store.util.page.PageResponse;
+import org.springframework.data.domain.PageRequest;
 
 public interface CategoryService {
     // GET
     CategoryDTOResponse getCategoryId (Long id);
+    PageResponse<CategoryDTOResponse> getPageListCategory (int page, int size);
 }
