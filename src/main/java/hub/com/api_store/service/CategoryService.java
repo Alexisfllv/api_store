610c5a9d781765f91e4960f38c6a2da730644e2b
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-01 23:24:05 -0500

    feat(category): CategoryService add method POST addCategory

--- src/main/java/hub/com/api_store/service/CategoryService.java
+++ src/main/java/hub/com/api_store/service/CategoryService.java
@@ -1,5 +1,6 @@
 package hub.com.api_store.service;
 
+import hub.com.api_store.dto.category.CategoryDTORequest;
 import hub.com.api_store.dto.category.CategoryDTOResponse;
 import hub.com.api_store.util.page.PageResponse;
 import org.springframework.data.domain.PageRequest;
@@ -8,4 +9,7 @@ public interface CategoryService {
     // GET
     CategoryDTOResponse getCategoryId (Long id);
     PageResponse<CategoryDTOResponse> getPageListCategory (int page, int size);
+
+    // POST
+    CategoryDTOResponse addCategory(CategoryDTORequest categoryDTORequest);
 }
