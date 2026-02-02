6c3e6534f0ea1531d2464aa34f1f5e29e195b205
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-01 23:18:02 -0500

    feat(category): dto update CategoryDTORequest validation and swagger

--- src/main/java/hub/com/api_store/dto/category/CategoryDTORequest.java
+++ src/main/java/hub/com/api_store/dto/category/CategoryDTORequest.java
@@ -1,9 +1,17 @@
 package hub.com.api_store.dto.category;
 
 import hub.com.api_store.nums.CategoryStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CategoryDTORequest(
+        @Schema(description = "Category name.", example = "Frutas", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "{field.required}")
+        @Size(min = 2, max = 100, message = "{field.size.range}")
         String name,
-        String description,
-        CategoryStatus status
+
+        @Schema(description = "Category description.", example = "Frutas frescas y de temporada",  requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+        @Size(max = 500, message = "{field.max.length}")
+        String description
 ) {}
