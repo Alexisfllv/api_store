00404becc2de305036f1e3bfb59e3a84e33eef37
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-03 14:50:23 -0500

    feat(category): CategoryService add method GET getPageListCategoryByStatus

--- src/main/java/hub/com/api_store/service/CategoryService.java
+++ src/main/java/hub/com/api_store/service/CategoryService.java
@@ -3,13 +3,16 @@ package hub.com.api_store.service;
 import hub.com.api_store.dto.category.CategoryDTORequest;
 import hub.com.api_store.dto.category.CategoryDTOResponse;
 import hub.com.api_store.dto.category.CategoryDTOUpdate;
+import hub.com.api_store.nums.CategoryStatus;
 import hub.com.api_store.util.page.PageResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 public interface CategoryService {
     // GET
     CategoryDTOResponse getCategoryId (Long id);
     PageResponse<CategoryDTOResponse> getPageListCategory (int page, int size);
+    PageResponse<CategoryDTOResponse> getPageListCategoryByStatus (CategoryStatus status, int page, int size);
 
     // POST
     CategoryDTOResponse addCategory(CategoryDTORequest categoryDTORequest);
