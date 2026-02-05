38db755224ed3081b67c8a63d6c56f3008363180
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-05 10:13:49 -0500

    feat(supplier) SupplierController add service GET getPageListSupplierGet

--- src/main/java/hub/com/api_store/controller/SupplierController.java
+++ src/main/java/hub/com/api_store/controller/SupplierController.java
@@ -2,15 +2,13 @@ package hub.com.api_store.controller;
 
 import hub.com.api_store.dto.supplier.SupplierDTOResponse;
 import hub.com.api_store.service.SupplierService;
+import hub.com.api_store.util.page.PageResponse;
 import hub.com.api_store.util.response.GenericResponse;
 import hub.com.api_store.util.response.StatusApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +19,16 @@ public class SupplierController {
     @GetMapping("/{id}")
     public ResponseEntity<GenericResponse<SupplierDTOResponse>> getSupplierIdGet(@PathVariable Long id){
         SupplierDTOResponse response = supplierService.getSupplierId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new GenericResponse<>(StatusApi.SUCCESS, response)
+        );
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<GenericResponse<PageResponse<SupplierDTOResponse>>> getPageListSupplierGet(
+            @RequestParam(defaultValue = "0")int page,
+            @RequestParam(defaultValue = "3")int size){
+        PageResponse<SupplierDTOResponse> response = supplierService.getPageListSupplier(page, size);
         return ResponseEntity.status(HttpStatus.OK).body(
                 new GenericResponse<>(StatusApi.SUCCESS, response)
         );
