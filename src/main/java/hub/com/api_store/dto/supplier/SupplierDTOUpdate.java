7092516acea9c14b337d59e80a00d8e2955a72f8
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-05 17:24:50 -0500

    feat(supplier): dto add SupplierDTOUpdate

---
+++ src/main/java/hub/com/api_store/dto/supplier/SupplierDTOUpdate.java
@@ -0,0 +1,34 @@
+package hub.com.api_store.dto.supplier;
+
+import hub.com.api_store.nums.CategoryStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record SupplierDTOUpdate(
+
+        @Schema(description = "Supplier name.", example = "Evans")
+        @NotBlank(message = "{field.required}")
+        @Size(min = 2, max = 100, message = "{field.size.range}")
+        String name,
+
+        @Schema(description = "Supplier phone.", example = "987654321", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "{field.required}")
+        @Size(min = 2, max = 12, message = "{field.size.range}")
+        String phone,
+
+        @Schema(description = "Supplier email.", example = "evans@email.com", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "{field.required}")
+        @Size(min = 2, max = 100, message = "{field.size.range}")
+        String email,
+
+        @Schema(description = "Supplier address.", example = "Lima...",requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "{field.required}")
+        @Size(min = 2, max = 200, message = "{field.size.range}")
+        String address,
+
+        @Schema(description = "Supplier status.", example = "ACTIVE",requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotNull(message = "{field.required}")
+        CategoryStatus status
+) { }
