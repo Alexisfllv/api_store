3dec74519643f981ec3f0c9a04b6aa748d523e5b
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-05 10:15:42 -0500

    feat(supplier): Supplier add service GET getPageListSupplier with Unit Test (#13)
    * feat(supplier): SupplierService add method GET getPageListSupplier
    
    * feat(supplier): SupplierServiceImpl implement method GET getPageListSupplier
    
    * feat(supplier) SupplierController add service GET getPageListSupplierGet
    
    * test(supplier): SupplierServiceImplTest add test service GET getPageListSupplier

--- src/main/java/hub/com/api_store/service/SupplierService.java
+++ src/main/java/hub/com/api_store/service/SupplierService.java
@@ -1,8 +1,11 @@
 package hub.com.api_store.service;
 
 import hub.com.api_store.dto.supplier.SupplierDTOResponse;
+import hub.com.api_store.util.page.PageResponse;
+import org.springframework.data.domain.PageRequest;
 
 public interface SupplierService {
     // GET
     SupplierDTOResponse getSupplierId(Long id);
+    PageResponse<SupplierDTOResponse> getPageListSupplier(int page, int size);
 }
--- src/test/java/hub/com/api_store/service/impl/SupplierServiceImplTest.java
+++ src/test/java/hub/com/api_store/service/impl/SupplierServiceImplTest.java
@@ -4,7 +4,9 @@ import hub.com.api_store.dto.supplier.SupplierDTOResponse;
 import hub.com.api_store.entity.Supplier;
 import hub.com.api_store.mapper.SupplierMapper;
 import hub.com.api_store.nums.CategoryStatus;
+import hub.com.api_store.repo.SupplierRepo;
 import hub.com.api_store.service.domain.SupplierServiceDomain;
+import hub.com.api_store.util.page.PageResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,8 +15,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -30,8 +37,23 @@ public class SupplierServiceImplTest {
     @Mock
     private SupplierServiceDomain supplierServiceDomain;
 
+    @Mock
+    private SupplierRepo supplierRepo;
+
     @InjectMocks
     private SupplierServiceImpl supplierServiceImpl;
+
+
+    // helper entity and dto
+    private Supplier createSupplier(Long id, String name, String phone, String email, String address, CategoryStatus status) {
+        return new Supplier(id, name, phone, email, address, status);
+    }
+
+    private SupplierDTOResponse createSupplierDTO(Long id, String name, String phone, String email, String address, CategoryStatus status) {
+        return new SupplierDTOResponse(id, name, phone, email, address, status);
+    }
+
+
 
     @Test
     @DisplayName("GET getSupplierId")
@@ -61,6 +83,44 @@ public class SupplierServiceImplTest {
         InOrder inOrder = Mockito.inOrder(supplierServiceDomain, supplierMapper);
         inOrder.verify(supplierServiceDomain).findByIdSupplier(supplierId);
         inOrder.verify(supplierMapper).toSupplierDTOResponse(supplier);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    @DisplayName("GET getPageListSupplier")
+    void getPageListSupplier(){
+        // Arrange
+        int page = 0;
+        int size = 10;
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC,"id"));
+        Supplier supplier1 = createSupplier(1L, "Fring", "+51920287650", "Fring@email.com", "Lima-Lima", CategoryStatus.ACTIVE);
+        Supplier supplier2 = createSupplier(2L, "West", "+51923431112", "West@email.com", "AQP-", CategoryStatus.INACTIVE);
+        List<Supplier> supplierList = List.of(supplier1, supplier2);
+        Page<Supplier> pageSupplier = new PageImpl(supplierList, pageRequest, supplierList.size());
+
+        SupplierDTOResponse dto1 = createSupplierDTO(1L, "Fring", "+51920287650", "Fring@email.com", "Lima-Lima", CategoryStatus.ACTIVE);
+        SupplierDTOResponse dto2 = createSupplierDTO(2L, "West", "+51923431112", "West@email.com", "AQP-", CategoryStatus.INACTIVE);
+
+        when(supplierRepo.findAll(pageRequest)).thenReturn(pageSupplier);
+        when(supplierMapper.toSupplierDTOResponse(supplier1)).thenReturn(dto1);
+        when(supplierMapper.toSupplierDTOResponse(supplier2)).thenReturn(dto2);
+        // Act
+        PageResponse<SupplierDTOResponse> result = supplierServiceImpl.getPageListSupplier(page,size);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(2,result.content().size()),
+                () -> assertEquals(0,result.page()),
+                () -> assertEquals(2,result.totalElements()),
+                () -> assertEquals(10,result.size()),
+                () -> assertEquals(1,result.totalPages())
+        );
+
+        // InOrder & Verify
+        InOrder inOrder = Mockito.inOrder(supplierRepo, supplierMapper);
+        inOrder.verify(supplierRepo).findAll(pageRequest);
+        inOrder.verify(supplierMapper).toSupplierDTOResponse(supplier1);
+        inOrder.verify(supplierMapper).toSupplierDTOResponse(supplier2);
         inOrder.verifyNoMoreInteractions();
     }
 }
