d66728b2cc371e2a32f520792421a9ec811d5bb3
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-05 10:06:56 -0500

    feat(supplier): SupplierServiceImpl implement method GET getPageListSupplier

--- src/main/java/hub/com/api_store/service/impl/SupplierServiceImpl.java
+++ src/main/java/hub/com/api_store/service/impl/SupplierServiceImpl.java
@@ -3,9 +3,14 @@ package hub.com.api_store.service.impl;
 import hub.com.api_store.dto.supplier.SupplierDTOResponse;
 import hub.com.api_store.entity.Supplier;
 import hub.com.api_store.mapper.SupplierMapper;
+import hub.com.api_store.repo.SupplierRepo;
 import hub.com.api_store.service.SupplierService;
 import hub.com.api_store.service.domain.SupplierServiceDomain;
+import hub.com.api_store.util.page.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +20,30 @@ public class SupplierServiceImpl implements SupplierService {
     private final SupplierMapper supplierMapper;
     private final SupplierServiceDomain supplierServiceDomain;
 
+    // strag
+    private final SupplierRepo supplierRepo;
+
     @Override
     public SupplierDTOResponse getSupplierId(Long id) {
         Supplier supplierExist = supplierServiceDomain.findByIdSupplier(id);
         SupplierDTOResponse response = supplierMapper.toSupplierDTOResponse(supplierExist);
         return response;
+    }
+
+    @Override
+    public PageResponse<SupplierDTOResponse> getPageListSupplier(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC,"id"));
+        Page<Supplier> pageSupplier = supplierRepo.findAll(pageRequest);
+
+        return new PageResponse<>(
+                pageSupplier.getContent()
+                        .stream()
+                        .map(supplierMapper::toSupplierDTOResponse)
+                        .toList(),
+                pageSupplier.getNumber(),
+                pageSupplier.getSize(),
+                pageSupplier.getTotalElements(),
+                pageSupplier.getTotalPages()
+        );
     }
 }
