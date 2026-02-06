03421332b662460bbcc9f4d58451bdc81bb564d6
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-06 14:52:21 -0500

    feat(supplier): SupplierService add method GET getListSupplierByStatus

--- src/main/java/hub/com/api_store/service/SupplierService.java
+++ src/main/java/hub/com/api_store/service/SupplierService.java
@@ -3,6 +3,7 @@ package hub.com.api_store.service;
 import hub.com.api_store.dto.supplier.SupplierDTORequest;
 import hub.com.api_store.dto.supplier.SupplierDTOResponse;
 import hub.com.api_store.dto.supplier.SupplierDTOUpdate;
+import hub.com.api_store.nums.CategoryStatus;
 import hub.com.api_store.util.page.PageResponse;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public interface SupplierService {
     SupplierDTOResponse getSupplierId(Long id);
     PageResponse<SupplierDTOResponse> getPageListSupplier(int page, int size);
     List<SupplierDTOResponse> getListSupplierByName(String name , Integer limit);
+    List<SupplierDTOResponse> getListSupplierByStatus(String status, Integer limit);
     // POST
     SupplierDTOResponse addSupplier(SupplierDTORequest supplierDTORequest);
 
