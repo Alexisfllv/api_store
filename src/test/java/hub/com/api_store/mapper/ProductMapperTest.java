3e7af7bab14d85c619604a66d7839ee996cd7c71
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-07 12:35:49 -0500

    test(product): ProductMapperTest add method test toProductDTOResponse

---
+++ src/test/java/hub/com/api_store/mapper/ProductMapperTest.java
@@ -0,0 +1,48 @@
+package hub.com.api_store.mapper;
+
+import hub.com.api_store.dto.product.ProductDTOResponse;
+import hub.com.api_store.entity.Category;
+import hub.com.api_store.entity.Product;
+import hub.com.api_store.nums.GlobalStatus;
+import hub.com.api_store.nums.GlobalUnit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductMapperTest {
+
+    private ProductMapper productMapper;
+
+    @BeforeEach
+    void setUp() {
+        productMapper = new ProductMapper();
+    }
+
+    @Nested
+    @DisplayName("Test mapped response <- entity")
+    class ToResponseTests {
+        @Test
+        @DisplayName("Should map response <- entity")
+        void toProductDTOResponse_shouldMapCorrectly() {
+            // Arrange
+            Category category = new Category(1L,"name","description", GlobalStatus.ACTIVE);
+            Product product = new Product(1L,"name",GlobalUnit.KG,GlobalStatus.ACTIVE,category);
+
+            // Act
+            ProductDTOResponse productDTOResponse = productMapper.toProductDTOResponse(product);
+            // Assert
+            assertAll(
+                    () -> assertNotNull(productDTOResponse),
+                    () -> assertEquals(1L, productDTOResponse.id()),
+                    () -> assertEquals("name", productDTOResponse.name()),
+                    () -> assertEquals(GlobalUnit.KG, productDTOResponse.unit()),
+                    () -> assertEquals(GlobalStatus.ACTIVE, productDTOResponse.status()),
+                    () -> assertEquals(1L, productDTOResponse.categoryId()),
+                    () -> assertEquals("name", productDTOResponse.categoryName())
+            );
+        }
+    }
+}
