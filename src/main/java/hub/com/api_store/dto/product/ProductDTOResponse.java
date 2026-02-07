8c7f4546d7bdc53b97a87aa074a2d618c51b88c6
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-07 12:28:55 -0500

    feat(product): dto update ProductDTOResponse unit Enum

--- src/main/java/hub/com/api_store/dto/product/ProductDTOResponse.java
+++ src/main/java/hub/com/api_store/dto/product/ProductDTOResponse.java
@@ -1,11 +1,12 @@
 package hub.com.api_store.dto.product;
 
 import hub.com.api_store.nums.GlobalStatus;
+import hub.com.api_store.nums.GlobalUnit;
 
 public record ProductDTOResponse (
             Long id,
             String name,
-            String unit,
+            GlobalUnit unit,
             GlobalStatus status,
             Long categoryId,
             String categoryName
