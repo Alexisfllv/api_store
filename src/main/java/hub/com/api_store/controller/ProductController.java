9e1a3b0e24c3788d77febf1940be6b2464d625ac
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-07 20:42:49 -0500

    feat(product) ProductController add service GET getPageFindAllProductsGet

--- src/main/java/hub/com/api_store/controller/ProductController.java
+++ src/main/java/hub/com/api_store/controller/ProductController.java
@@ -2,15 +2,15 @@ package hub.com.api_store.controller;
 
 import hub.com.api_store.dto.product.ProductDTOResponse;
 import hub.com.api_store.service.ProductService;
+import hub.com.api_store.util.page.PageResponse;
 import hub.com.api_store.util.response.GenericResponse;
 import hub.com.api_store.util.response.StatusApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     private final ProductService productService;
 
+    // GET
     @GetMapping("/{id}")
     public ResponseEntity<GenericResponse<ProductDTOResponse>> getProductByIdGet(@PathVariable Long id){
         ProductDTOResponse productDTOResponse = productService.getProductById(id);
         return ResponseEntity.status(HttpStatus.OK).body(
                 new GenericResponse<>(StatusApi.SUCCESS, productDTOResponse)
         );
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<GenericResponse<PageResponse<ProductDTOResponse>>> getPageFindAllProductsGet(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String prop
+            ){
+        PageResponse<ProductDTOResponse> pageResponse = productService.getPageFindAllProducts(page, size, prop);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new GenericResponse<>(StatusApi.SUCCESS, pageResponse)
+        );
+
     }
 }
