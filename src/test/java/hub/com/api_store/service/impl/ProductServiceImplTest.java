396f40e4f117bdf45f00b8a795a6be5c6e1ee770
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-07 21:09:09 -0500

    test(product): ProductServiceImplTest add service test GET getPageFindAllProductsGet

--- src/test/java/hub/com/api_store/service/impl/ProductServiceImplTest.java
+++ src/test/java/hub/com/api_store/service/impl/ProductServiceImplTest.java
@@ -6,7 +6,9 @@ import hub.com.api_store.entity.Product;
 import hub.com.api_store.mapper.ProductMapper;
 import hub.com.api_store.nums.GlobalStatus;
 import hub.com.api_store.nums.GlobalUnit;
+import hub.com.api_store.repo.ProductRepo;
 import hub.com.api_store.service.domain.ProductServiceDomain;
+import hub.com.api_store.util.page.PageResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,6 +35,9 @@ public class ProductServiceImplTest {
 
     @Mock
     private ProductMapper productMapper;
+
+    @Mock
+    private ProductRepo productRepo;
 
     @InjectMocks
     private ProductServiceImpl productServiceImpl;
@@ -61,5 +70,44 @@ public class ProductServiceImplTest {
         inOrder.verify(productMapper).toProductDTOResponse(product);
         inOrder.verifyNoMoreInteractions();
 
+    }
+
+    @Test
+    @DisplayName("getPageFindAllProducts Test")
+    void getPageFindAllProductsGet(){
+        // Arrange
+        int page = 0;
+        int size = 10;
+        String prop = "id";
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, prop));
+        Category category = new Category(1L,"name","description", GlobalStatus.ACTIVE);
+        Product product = new Product(1L,"name", GlobalUnit.KG,GlobalStatus.ACTIVE,category);
+        List<Product> productList = List.of(product);
+        ProductDTOResponse productDTOResponse = new ProductDTOResponse(1L,"name", GlobalUnit.KG,GlobalStatus.ACTIVE,product.getCategory().getId(),product.getCategory().getName());
+        Page<Product> pageProducts = new PageImpl<>(productList,pageable,productList.size());
+
+        when(productRepo.findAll(pageable)).thenReturn(pageProducts);
+        when(productMapper.toProductDTOResponse(product)).thenReturn(productDTOResponse);
+        // Act
+        PageResponse<ProductDTOResponse> resultList = productServiceImpl.getPageFindAllProducts(page,size,prop);
+        // Assert
+        assertAll(
+                () -> assertEquals(1,resultList.content().size()),
+                () -> assertEquals(productDTOResponse.id(),resultList.content().get(0).id()),
+                () -> assertEquals(productDTOResponse.name(),resultList.content().get(0).name()),
+                () -> assertEquals(productDTOResponse.unit(),resultList.content().get(0).unit()),
+                () -> assertEquals(productDTOResponse.status(),resultList.content().get(0).status()),
+                () -> assertEquals(productDTOResponse.categoryId(),resultList.content().get(0).categoryId()),
+                () -> assertEquals(productDTOResponse.categoryName(),resultList.content().get(0).categoryName()),
+                () -> assertEquals(page,resultList.page()),
+                () -> assertEquals(size,resultList.size()),
+                () -> assertEquals(productList.size(),resultList.totalElements()),
+                () -> assertEquals(1,resultList.totalPages())
+        );
+        // InOrder & Verify
+        InOrder inOrder = Mockito.inOrder(productRepo, productMapper);
+        inOrder.verify(productRepo).findAll(pageable);
+        inOrder.verify(productMapper).toProductDTOResponse(product);
+        inOrder.verifyNoMoreInteractions();
     }
 }
