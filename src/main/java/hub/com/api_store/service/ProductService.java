c094f913283ef6d7c8a53b5016bafdfe150b7394
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-07 21:24:48 -0500

    feat(product): Product add service GET getPageFindAllProductsGet with Unit Test (#20)
    * feat(product): Product add method GET  getPageFindAllProducts
    
    * feat(product): ProductServiceImpl implement method GET getPageFindAllProducts
    
    * feat(product) ProductController add service GET getPageFindAllProductsGet
    
    * test(product): ProductServiceImplTest add service test GET getPageFindAllProductsGet

--- src/main/java/hub/com/api_store/service/ProductService.java
+++ src/main/java/hub/com/api_store/service/ProductService.java
@@ -1,8 +1,10 @@
 package hub.com.api_store.service;
 
 import hub.com.api_store.dto.product.ProductDTOResponse;
+import hub.com.api_store.util.page.PageResponse;
 
 public interface ProductService {
     // GET
     ProductDTOResponse getProductById(Long id);
+    PageResponse<ProductDTOResponse> getPageFindAllProducts(int page, int size,String prop);
 }
