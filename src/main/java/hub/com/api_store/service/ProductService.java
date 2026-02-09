8447d55f737423cc370710a320e405188f2d085e
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-09 13:40:11 -0500

    feat(product): ProducService add method PUT updateProduct

--- src/main/java/hub/com/api_store/service/ProductService.java
+++ src/main/java/hub/com/api_store/service/ProductService.java
@@ -2,6 +2,7 @@ package hub.com.api_store.service;
 
 import hub.com.api_store.dto.product.ProductDTORequest;
 import hub.com.api_store.dto.product.ProductDTOResponse;
+import hub.com.api_store.dto.product.ProductDTOUpdate;
 import hub.com.api_store.util.page.PageResponse;
 
 public interface ProductService {
@@ -11,4 +12,7 @@ public interface ProductService {
 
     // POST
     ProductDTOResponse addProduct(ProductDTORequest productDTORequest);
+
+    // PUT
+    ProductDTOResponse updateProduct(Long id, ProductDTOUpdate productDTOUpdate);
 }
