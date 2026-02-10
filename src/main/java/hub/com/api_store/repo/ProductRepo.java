776abb69c6f2a4b547524b65f021e8bdbe9d9826
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-09 19:11:24 -0500

    feat(product): ProductRepo add method findByCategoryId

--- src/main/java/hub/com/api_store/repo/ProductRepo.java
+++ src/main/java/hub/com/api_store/repo/ProductRepo.java
@@ -3,5 +3,9 @@ package hub.com.api_store.repo;
 import hub.com.api_store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+
 }
