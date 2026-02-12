2e6b962c10cbe80d91a3ea70bd6f350d5f579c60
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-12 10:37:35 -0500

    feat(purchase): PurchaseService add method GET findAllListPagePurchase

--- src/main/java/hub/com/api_store/service/PurchaseService.java
+++ src/main/java/hub/com/api_store/service/PurchaseService.java
@@ -1,9 +1,12 @@
 package hub.com.api_store.service;
 
 import hub.com.api_store.dto.purchase.PurchaseDTOResponse;
+import hub.com.api_store.util.page.PageResponse;
+import org.springframework.data.domain.PageRequest;
 
 public interface PurchaseService {
 
     // GET
     PurchaseDTOResponse findByPurchaseId(Long id);
+    PageResponse<PurchaseDTOResponse> findAllListPagePurchase(int page, int size,String prop);
 }
