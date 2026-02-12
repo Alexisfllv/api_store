6eda542e4042ec1b33eafaa5c05889797971470a
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-12 10:39:04 -0500

    feat(purchase): PurchaseController add service GET findAllListPagePurchaseGet

--- src/main/java/hub/com/api_store/controller/PurchaseController.java
+++ src/main/java/hub/com/api_store/controller/PurchaseController.java
@@ -2,15 +2,13 @@ package hub.com.api_store.controller;
 
 import hub.com.api_store.dto.purchase.PurchaseDTOResponse;
 import hub.com.api_store.service.PurchaseService;
+import hub.com.api_store.util.page.PageResponse;
 import hub.com.api_store.util.response.GenericResponse;
 import hub.com.api_store.util.response.StatusApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +17,23 @@ public class PurchaseController {
 
     private final PurchaseService purchaseService;
 
+    // GET
     @GetMapping("/{id}")
     public ResponseEntity<GenericResponse<PurchaseDTOResponse>> findByPurchaseIdGet(@PathVariable Long id){
         PurchaseDTOResponse purchaseDTOResponse = purchaseService.findByPurchaseId(id);
         return ResponseEntity.status(HttpStatus.OK).body(
                 new GenericResponse<>(StatusApi.SUCCESS, purchaseDTOResponse)
+        );
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<GenericResponse<PageResponse<PurchaseDTOResponse>>> findAllListPagePurchaseGet(
+            @RequestParam (defaultValue = "0") Integer page,
+            @RequestParam (defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "id") String prop){
+        PageResponse<PurchaseDTOResponse> pageResponse = purchaseService.findAllListPagePurchase(page, size, prop);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new GenericResponse<>(StatusApi.SUCCESS, pageResponse)
         );
     }
 
