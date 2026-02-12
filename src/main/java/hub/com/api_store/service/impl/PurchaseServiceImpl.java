0250c50f25a6611d89ef788c5f07e1eda1d49b0e
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-12 10:38:40 -0500

    feat(purchase): PurchaseServiceImpl implement method GET findAllListPagePurchase

--- src/main/java/hub/com/api_store/service/impl/PurchaseServiceImpl.java
+++ src/main/java/hub/com/api_store/service/impl/PurchaseServiceImpl.java
@@ -6,7 +6,12 @@ import hub.com.api_store.mapper.PurchaseMapper;
 import hub.com.api_store.repo.PurchaseRepo;
 import hub.com.api_store.service.PurchaseService;
 import hub.com.api_store.service.domain.PurchaseServiceDomain;
+import hub.com.api_store.util.page.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,11 +25,28 @@ public class PurchaseServiceImpl implements PurchaseService {
     private final PurchaseMapper purchaseMapper;
     private final PurchaseServiceDomain purchaseServiceDomain;
 
+    // GET
     @Override
     public PurchaseDTOResponse findByPurchaseId(Long id) {
         Purchase purchaseExist = purchaseServiceDomain.findPurchaseById(id);
         PurchaseDTOResponse purchaseDTOResponse = purchaseMapper.toPurchaseDTOResponse(purchaseExist);
         return purchaseDTOResponse;
+    }
+
+    @Override
+    public PageResponse<PurchaseDTOResponse> findAllListPagePurchase(int page, int size,String prop) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, prop));
+        Page<Purchase> purchasePageResponse = purchaseRepo.findAll(pageable);
+        return new PageResponse<>(
+                purchasePageResponse.getContent()
+                        .stream()
+                        .map(purchaseMapper::toPurchaseDTOResponse)
+                        .toList(),
+                purchasePageResponse.getNumber(),
+                purchasePageResponse.getSize(),
+                purchasePageResponse.getTotalElements(),
+                purchasePageResponse.getTotalPages()
+        );
     }
 
 }
