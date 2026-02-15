fd39a40ce573d163edb0c7ea22e3e339dd474354
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-14 22:41:11 -0500

    feat(inventory): InventoryRepo add method findByProductAndLotAndWarehouse

---
+++ src/main/java/hub/com/api_store/repo/InventoryRepo.java
@@ -0,0 +1,17 @@
+package hub.com.api_store.repo;
+
+import hub.com.api_store.entity.Inventory;
+import hub.com.api_store.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface InventoryRepo extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProductAndLotAndWarehouse(
+            Product product,
+            String lot,
+            String warehouse
+    );
+
+}
