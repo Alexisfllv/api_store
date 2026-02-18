a8ca14bc06ce9c5934d322ff54e42fe6a9dc25d2
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-18 10:29:19 -0500

    feat(purchase): PurchaseRepo add method findByPurchaseDateBetween

--- src/main/java/hub/com/api_store/repo/PurchaseRepo.java
+++ src/main/java/hub/com/api_store/repo/PurchaseRepo.java
@@ -3,9 +3,12 @@ package hub.com.api_store.repo;
 import hub.com.api_store.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PurchaseRepo extends JpaRepository<Purchase, Long> {
     List<Purchase> findByProductId(Long productId);
     List<Purchase> findBySupplierId(Long supplierId);
+
+    List<Purchase> findByPurchaseDateBetween(LocalDateTime start, LocalDateTime end);
 }
