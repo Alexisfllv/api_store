81676a15f2f8d82412e6abd77f605d4ec64db599
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-21 13:22:04 -0500

    feat(inventory): InventorryService add method GET findAllListPageInventory

--- src/main/java/hub/com/api_store/service/InventoryService.java
+++ src/main/java/hub/com/api_store/service/InventoryService.java
@@ -1,8 +1,17 @@
 package hub.com.api_store.service;
 
+import hub.com.api_store.dto.inventory.InventoryDTOResponse;
 import hub.com.api_store.entity.Inventory;
 import hub.com.api_store.entity.Purchase;
+import hub.com.api_store.util.page.PageResponse;
 
 public interface InventoryService {
+
+    // GET
+    PageResponse<InventoryDTOResponse> findAllListPageInventory(int page, int size, String prop);
+
+
+    // POST
     Inventory addStockFromPurchase(Purchase purchase);
+
 }
