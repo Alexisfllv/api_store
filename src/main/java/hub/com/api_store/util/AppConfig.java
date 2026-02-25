f240b71e2ea661557d016369d3159591a8d2278e
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-25 12:57:04 -0500

    chore(appconfig): AppConfig bean Clock defaultZone

---
+++ src/main/java/hub/com/api_store/util/AppConfig.java
@@ -0,0 +1,14 @@
+package hub.com.api_store.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+}
