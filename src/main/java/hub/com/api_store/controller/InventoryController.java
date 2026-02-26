5ac1d9d39e780fb6e37fbf43e88648491445ffde
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-26 13:00:59 -0500

    feat(inventory): InventoryController add service GET findInventoryExpiringBetweenGet

--- src/main/java/hub/com/api_store/controller/InventoryController.java
+++ src/main/java/hub/com/api_store/controller/InventoryController.java
@@ -6,10 +6,12 @@ import hub.com.api_store.util.page.PageResponse;
 import hub.com.api_store.util.response.GenericResponse;
 import hub.com.api_store.util.response.StatusApi;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -83,6 +85,17 @@ public class InventoryController {
     @GetMapping("/available")
     public ResponseEntity<GenericResponse<List<InventoryDTOResponse>>> findAvailableInventoryGet() {
         List<InventoryDTOResponse> response = inventoryService.findAvailableInventory();
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new GenericResponse<>(StatusApi.SUCCESS, response)
+        );
+    }
+
+    @GetMapping("/expiration/range")
+    public ResponseEntity<GenericResponse<List<InventoryDTOResponse>>> findInventoryExpiringBetweenGet(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end
+    ) {
+        List<InventoryDTOResponse> response = inventoryService.findInventoryExpiringBetween(start, end);
         return ResponseEntity.status(HttpStatus.OK).body(
                 new GenericResponse<>(StatusApi.SUCCESS, response)
         );
