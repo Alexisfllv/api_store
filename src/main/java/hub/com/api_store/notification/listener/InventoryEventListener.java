0c9be4120cf962b75aa90c89d47926bbe27d5995
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-02-26 23:35:13 -0500

    feat(notification): InventoryEventListener add method handleExpirationAlert sending and connect job to service

--- src/main/java/hub/com/api_store/notification/listener/InventoryEventListener.java
+++ src/main/java/hub/com/api_store/notification/listener/InventoryEventListener.java
@@ -1,4 +1,19 @@
 package hub.com.api_store.notification.listener;
 
+import hub.com.api_store.notification.event.ExpirationAlertEvent;
+import hub.com.api_store.notification.service.NotificationEventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class InventoryEventListener {
+
+    private final NotificationEventService notificationService;
+
+    @EventListener
+    public void handleExpirationAlert(ExpirationAlertEvent event) {
+        notificationService.sendExpirationAlert(event);
+    }
 }
