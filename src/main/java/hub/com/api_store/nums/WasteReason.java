0dad7a45fd073a77d5e09332c8233f01e3111ead
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-03-02 11:55:18 -0500

    feat(waste): add enum WasteReason

---
+++ src/main/java/hub/com/api_store/nums/WasteReason.java
@@ -0,0 +1,8 @@
+package hub.com.api_store.nums;
+
+public enum WasteReason {
+    EXPIRED,    // venció
+    DAMAGED,    // llegó dañado o se dañó
+    LOST,       // se perdió
+    OTHER       // cualquier otra razón
+}
