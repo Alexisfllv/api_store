18cad032a00ec897f8f1eeb29584098bc009c2c8
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-03-03 21:35:21 -0500

    feat(waste): WasteService add service method GET findAllPage

--- src/main/java/hub/com/api_store/service/WasteService.java
+++ src/main/java/hub/com/api_store/service/WasteService.java
@@ -2,8 +2,14 @@ package hub.com.api_store.service;
 
 import hub.com.api_store.dto.waste.WasteDTORequest;
 import hub.com.api_store.dto.waste.WasteDTOResponse;
+import hub.com.api_store.util.page.PageResponse;
+import org.springframework.data.domain.PageRequest;
 
 public interface WasteService {
+
+    // GET
+    PageResponse<WasteDTOResponse> findAllPage(int page, int size, String prop);
+
 
     // POST
     WasteDTOResponse createWaste(WasteDTORequest wasteDTORequest);
