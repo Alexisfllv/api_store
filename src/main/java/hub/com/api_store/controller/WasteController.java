159592c038d564ca3e366063aeb352c117832e7c
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-03-03 21:36:56 -0500

    feat(waste): WasteController add service GET findAllPageGet

--- src/main/java/hub/com/api_store/controller/WasteController.java
+++ src/main/java/hub/com/api_store/controller/WasteController.java
@@ -3,22 +3,33 @@ package hub.com.api_store.controller;
 import hub.com.api_store.dto.waste.WasteDTORequest;
 import hub.com.api_store.dto.waste.WasteDTOResponse;
 import hub.com.api_store.service.WasteService;
+import hub.com.api_store.util.page.PageResponse;
 import hub.com.api_store.util.response.GenericResponse;
 import hub.com.api_store.util.response.StatusApi;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/wastes")
 public class WasteController {
     private final WasteService wasteService;
+
+    // GET
+    @GetMapping("/page")
+    public ResponseEntity<GenericResponse<PageResponse<WasteDTOResponse>>> findAllPageGet(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String prop
+    ) {
+        PageResponse<WasteDTOResponse> response = wasteService.findAllPage(page, size, prop);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new GenericResponse<>(StatusApi.SUCCESS, response));
+    }
 
     // Post
     @PostMapping
