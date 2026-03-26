dae68301f72be4f2ed6a5264514ca144f54e116d
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-03-26 14:29:24 -0500

    feat(waste): WasteController add service GET findAllByReasonGet

--- src/main/java/hub/com/api_store/controller/WasteController.java
+++ src/main/java/hub/com/api_store/controller/WasteController.java
@@ -2,6 +2,7 @@ package hub.com.api_store.controller;
 
 import hub.com.api_store.dto.waste.WasteDTORequest;
 import hub.com.api_store.dto.waste.WasteDTOResponse;
+import hub.com.api_store.nums.WasteReason;
 import hub.com.api_store.service.WasteService;
 import hub.com.api_store.util.page.PageResponse;
 import hub.com.api_store.util.response.GenericResponse;
@@ -12,6 +13,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,6 +39,15 @@ public class WasteController {
         WasteDTOResponse response = wasteService.findByIdWaste(id);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new GenericResponse<>(StatusApi.SUCCESS,response));
+    }
+
+    @GetMapping("/by-reason")
+    public ResponseEntity<GenericResponse<List<WasteDTOResponse>>> findAllByReasonGet(
+            @RequestParam WasteReason reason
+            ){
+        List<WasteDTOResponse> res = wasteService.findAllWasteByReason(reason);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new GenericResponse<>(StatusApi.SUCCESS, res));
     }
 
     // Post
