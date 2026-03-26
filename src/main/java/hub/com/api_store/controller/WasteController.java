95545e5c7796d0e65ea6179debbb2900457d4233
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-03-26 15:51:28 -0500

    feat(waste): WasteController add service GET findAllByDateGet

--- src/main/java/hub/com/api_store/controller/WasteController.java
+++ src/main/java/hub/com/api_store/controller/WasteController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -48,6 +49,15 @@ public class WasteController {
         List<WasteDTOResponse> res = wasteService.findAllWasteByReason(reason);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new GenericResponse<>(StatusApi.SUCCESS, res));
+    }
+
+    @GetMapping("/by-date")
+    public ResponseEntity<GenericResponse<List<WasteDTOResponse>>> findAllByDateGet(
+            @RequestParam LocalDateTime start,
+            @RequestParam LocalDateTime end){
+        List<WasteDTOResponse> responseList = wasteService.findAllWasteByWasteDateBetween(start, end);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new GenericResponse<>(StatusApi.SUCCESS, responseList));
     }
 
     // Post
